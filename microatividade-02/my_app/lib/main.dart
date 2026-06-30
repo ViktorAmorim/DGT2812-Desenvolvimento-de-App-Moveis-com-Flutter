2fbda1d8ed4b9d18fcdb8015326d7674fac15e92
ViktorAmorim <joao.victor.amorim.martins@hotmail.com>
2026-06-30 18:09:57 -0300

    feat: Commit microatividade02 completo do zero

---
+++ microatividade-02/my_app/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(const MaterialApp(
+    home: StatelessWidgetExemplo("Olá Flutter - MaterialApp"),
+  ));
+}
+
+class StatelessWidgetExemplo extends StatelessWidget {
+  final String _appBarTitle;
+
+  const StatelessWidgetExemplo(this._appBarTitle, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appBarTitle),
+      ),
+      body: const Center(
+        child: Text("Macoratti .net"),
+      ),
+    );
+  }
+}
